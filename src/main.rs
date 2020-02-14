4626e20b52f06102f8fc1b8a6a7c70ac9a5547a9
Chris Meyering <christophe.meyering@gmail.com>
2020-02-13 16:57:41 -0800

    removed useless code

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,8 @@
+extern crate cryptochain;
 
-mod block;
+use cryptochain::blockchain::Blockchain;
 
-use block::Block;
-
+#[cfg_attr(tarpaulin, skip)]
 fn main() {
-    let block = Block::genesis();
-    println!("{:#?}", block);
-
-    let mut last_block = block;
-    for i in 0..4 {
-        let new_block = Block::mine_block(&last_block, String::from(format!("block {}", i).as_str()));
-        println!("{:#?}", new_block);
-        last_block = new_block;
-    }
+    let blockchain = Blockchain::new();
 }
