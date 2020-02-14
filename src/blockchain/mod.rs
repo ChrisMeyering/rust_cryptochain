f45235ac419ff5cd3ce524166c3a3ab14a4fc737
Chris Meyering <christophe.meyering@gmail.com>
2020-02-13 16:56:40 -0800

    implemented add_block for Blockchain

--- src/blockchain/mod.rs
+++ src/blockchain/mod.rs
@@ -3,14 +3,31 @@ mod block;
 use block::Block;
 
 pub struct Blockchain {
-    pub chain: Vec<Block>
+    pub chain: Vec<Block>,
 }
 
 impl Blockchain {
-    fn new() -> Blockchain {
+    pub fn new() -> Blockchain {
         Blockchain {
-            chain: vec![Block::genesis()];
+            chain: vec![Block::genesis()],
         }
     }
 
+    pub fn add_block(&mut self, data: String) {
+        let new_block = Block::mine_block(&self.chain[self.chain.len() - 1], data);
+        self.chain.push(new_block);
+    }
+
+
+}
+
+#[cfg(test)]
+mod blockchain_new {
+    use super::{Block, Blockchain};
+
+    #[test]
+    fn new_initializes_chain_properly() {
+        let blockchain = Blockchain::new();
+        assert_eq!(blockchain.chain[0], Block::genesis());
+    }
 }
